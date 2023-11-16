cffcc6b00677d8b3d13fd124f77c2df05c4a8dab
jean-benoist <jean-benoist.brault@sfr.fr>
2023-11-16 15:19:52 +0100

    the jsonifyRange method for testing works now to test wether i can load this module from git

--- remoteModuleTest.js
+++ remoteModuleTest.js
@@ -1,6 +1,11 @@
 async function jsonifyRange(excel){
-    let currentlySelectedRange = excel.workbook.getSelectedRange();
-    currentlySelectedRange.load('values');
+    // get currently selected range
+    let selRange = excel.workbook.getSelectedRange();
+    // load the values property
+    selRange.load(['values']);
     await excel.sync();
-    
-}
+    // transform the to json (note if i dont place selRange.values in a variable it doesn't work bug maybe ???)
+    let rangeValues = selRange.values;
+    // output to JADE console
+    Jade.print(JSON.stringify(rangeValues), 'JSON VALUES');
+  }
